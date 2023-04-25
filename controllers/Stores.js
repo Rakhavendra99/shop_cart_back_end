d3c1d9d264928b5ccf4a31c038d7128a4b1ac1ea
Rakhavendra S M <rakhavendra631999@gmail.com>
2023-04-25 11:58:12 +0530

    Edit Store is completed.

--- controllers/Stores.js
+++ controllers/Stores.js
@@ -1,7 +1,3 @@
-import Category from "../models/CategoryModel.js";
-import Product from "../models/ProductModel.js";
-import User from "../models/UserModel.js";
-import { Op } from "sequelize";
 import { getParamsParser, postRequestParser } from "../util/index.js";
 import Stores from "../models/StoreModel.js";
 
@@ -14,16 +10,16 @@ export const getStores = async (req, res) => {
     }
 }
 
-export const getProductById = async (req, res) => {
+export const getStoreById = async (req, res) => {
     const data = getParamsParser(req)
     try {
-        const product = await Product.findOne({
+        const findStore = await Stores.findOne({
             where: {
                 id: data.id
             }
         });
-        if (!product) return res.status(403).json({ msg: "Product Id not found" });
-        res.status(200).json(product);
+        if (!findStore) return res.status(403).json({ msg: "Store Id not found" });
+        res.status(200).json(findStore);
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
@@ -48,71 +44,34 @@ export const createStore = async (req, res) => {
     }
 }
 
-export const updateProduct = async (req, res) => {
+export const updateStore = async (req, res) => {
     const params = getParamsParser(req)
     const data = postRequestParser(req)
     try {
-        const product = await Product.findOne({
+        const findStore = await Stores.findOne({
             where: {
                 id: params.id
             }
         });
-        if (!product) return res.status(403).json({ msg: "Product not found" });
-        await product.update(Object.assign({}, data))
-        res.status(200).json({ msg: product });
+        if (!findStore) return res.status(403).json({ msg: "Store Id not found" });
+        await findStore.update(Object.assign({}, data))
+        res.status(200).json({ msg: findStore });
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
 }
 
-export const deleteProduct = async (req, res) => {
+export const deleteStore = async (req, res) => {
     const params = getParamsParser(req)
     try {
-        const product = await Product.findOne({
+        const findStore = await Stores.findOne({
             where: {
                 id: params.id
             }
         });
-        if (!product) return res.status(403).json({ msg: "Product id not found" });
-        await product.destroy(Object.assign({}, params))
-        res.status(200).json({ msg: product });
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-}
-export const getProductCategory = async (req, res) => {
-    try {
-        let response = await Category.findAll({
-            attributes: ['id', 'name'],
-            where: {
-                isActive: 1
-            }
-        });
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-}
-export const getCustomerProducts = async (req, res) => {
-    try {
-        let response = await Product.findAll({
-            include: Category
-        });
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-}
-export const getCustomerProductById = async (req, res) => {
-    const data = getParamsParser(req)
-    try {
-        const product = await Product.findOne({
-            where: {
-                id: data.id
-            }
-        });
-        if (!product) return res.status(403).json({ msg: "Product Id not found" });
-        res.status(200).json(product);
+        if (!findStore) return res.status(403).json({ msg: "Store id not found" });
+        await findStore.destroy(Object.assign({}, params))
+        res.status(200).json({ msg: findStore });
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
--- routes/StoreRoute.js
+++ routes/StoreRoute.js
@@ -1,28 +1,13 @@
 import express from "express";
-import {
-    getProducts,
-    getProductById,
-    createProduct,
-    updateProduct,
-    deleteProduct,
-    getProductCategory,
-    getCustomerProducts,
-    getCustomerProductById
-} from "../controllers/Products.js";
 import { verifyUser } from "../middleware/AuthUser.js";
-import { createStore, getStores } from "../controllers/Stores.js";
+import { createStore, deleteStore, getStoreById, getStores, updateStore } from "../controllers/Stores.js";
 
 const router = express.Router();
 
 router.get('/stores',verifyUser, getStores);
-router.get('/stores/category',verifyUser, getProductCategory);
-router.get('/stores/:id',verifyUser, getProductById);
+router.get('/stores/:id',verifyUser, getStoreById);
 router.post('/stores',verifyUser, createStore);
-router.patch('/stores/:id',verifyUser, updateProduct);
-router.delete('/stores/:id',verifyUser, deleteProduct);
-
-
-router.get('/customer/products',getCustomerProducts);
-router.get('/customer/products/:id',verifyUser, getCustomerProductById);
+router.patch('/stores/:id',verifyUser, updateStore);
+router.delete('/stores/:id',verifyUser, deleteStore);
 
 export default router;
