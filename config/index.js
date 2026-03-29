a8807a8a4b82c1ec774ed1f30a847f2d29ea778d
Rakhavendra S M <rakhavendra6319999@gmail.com>
2026-03-29 21:43:14 +0530

    Reapply "cooking vendor role added"
    This reverts commit 0bdf6eff404a2b72ea10cd074e3411597e56c90a.

--- config/index.js
+++ config/index.js
@@ -18,10 +18,10 @@ function value() {
                 limit: 100,
                 offset: 0,
                 isShowOTP: 0,
-                // stripe: {
-                //     secretKey: "test",
-                //     webhookSecret: "whsec_YaojvuF7o3V4M7jnrVPt9EPmvJ3Y0K8Z"
-                // }
+                stripe: {
+                    secretKey: "",
+                    webhookSecret: ""
+                }
             };
         case 'local':
             return {
@@ -36,10 +36,10 @@ function value() {
                 limit: 100,
                 offset: 0,
                 isShowOTP: 0,
-                // stripe: {
-                //     secretKey: "test",
-                //     webhookSecret: "whsec_YaojvuF7o3V4M7jnrVPt9EPmvJ3Y0K8Z"
-                // }
+                stripe: {
+                    secretKey: "",
+                    webhookSecret: ""
+                }
             };
         case 'production':
             return {
@@ -54,10 +54,10 @@ function value() {
                 limit: 100,
                 offset: 0,
                 isShowOTP: 0,
-                // stripe: {
-                //     secretKey: "test",
-                //     webhookSecret: "whsec_YaojvuF7o3V4M7jnrVPt9EPmvJ3Y0K8Z"
-                // }
+                stripe: {
+                    secretKey: "",
+                    webhookSecret: ""
+                }
             };
         default:
             return {
@@ -72,10 +72,10 @@ function value() {
                 limit: 100,
                 offset: 0,
                 isShowOTP: 0,
-                // stripe: {
-                //     secretKey: "test",
-                //     webhookSecret: "whsec_YaojvuF7o3V4M7jnrVPt9EPmvJ3Y0K8Z"
-                // }
+                stripe: {
+                    secretKey: "",
+                    webhookSecret: ""
+                }
             };
     }
 };
